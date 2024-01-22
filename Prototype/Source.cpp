998fac4781ba74b502cb24b3f8d271dee1e3dfb5
Mira Madani <miramadani2701@gmail.com>
2024-01-22 20:43:28 +0200

    f

--- Prototype/Source.cpp
+++ Prototype/Source.cpp
@@ -5,8 +5,7 @@
 using namespace std;
 
 
-// Абстрактный класс аппаратного устройства
-class AbstractDevice abstract
+class Part abstract
 {
 protected:
 	string producer;
@@ -20,455 +19,194 @@ public:
 	{
 		this->producer = producer;
 	}
- // Метод создания клона объекта
-	virtual AbstractDevice* Clone() abstract;
+	// Метод создания клона объекта
+	virtual Part* Clone() abstract;
 };
 
-class Box : public AbstractDevice
+class Engine : public Part
 {
-	string color;
-public:
-	string GetColor()
-	{
-		return this->color;
-	}
-	void SetColor(string color)
-	{
-		this->color = color;
-	}
-	AbstractDevice* Clone() override
-	{
-		Box * box = new Box;
-		*box = *this;
-		return box;
-	}
-};
-
-class Memory : public AbstractDevice
-{
-	int volume;
 	string type;
 public:
 	string GetType()
 	{
 		return this->type;
 	}
-	void SetType(string type)
+	void SetType(string t)
 	{
-		this->type = type;
+		this->type = t;
 	}
-	int GetVolume()
+	Part* Clone() override
 	{
-		return this->volume;
-	}
-	void SetVolume(int volume)
-	{
-		this->volume = volume;
-	}
-	AbstractDevice* Clone() override
-	{
-		Memory * memory = new Memory;
-		*memory = *this;
-		return memory;
+		Engine* eng = new Engine;
+		*eng = *this;
+		return eng;
 	}
 };
 
-class Hdd : public AbstractDevice
+class gearBox : public Part
 {
-	int volume;
+	bool avtomat;
+public:
+	bool GetAvtomat()
+	{
+		return this->avtomat;
+	}
+	void SetAvtomat(bool t)
+	{
+		this->avtomat = t;
+	}
+	Part* Clone() override
+	{
+		gearBox* eng = new gearBox;
+		*eng = *this;
+		return eng;
+	}
+};
+class Fluel : public Part
+{
 	string type;
 public:
 	string GetType()
 	{
 		return this->type;
 	}
-	void SetType(string type)
+	void SetType(string t)
 	{
-		this->type = type;
+		this->type = t;
 	}
-	int GetVolume()
+	Part* Clone() override
 	{
-		return this->volume;
-	}
-	void SetVolume(int volume)
-	{
-		this->volume = volume;
-	}
-	AbstractDevice* Clone() override
-	{
-		Hdd * hdd = new Hdd;
-		*hdd = *this;
-		return hdd;
+		Fluel* eng = new Fluel;
+		*eng = *this;
+		return eng;
 	}
 };
 
-class Processor : public AbstractDevice
+
+class Clearence : public Part
 {
-	int corecount;
-	double frequency;
+	int size;
 public:
-	double GetFrequency()
+	int GetSize()
 	{
-		return this->frequency;
+		return this->size;
 	}
-	void SetFrequency(double frequency)
+	void SetSize(int t)
 	{
-		this->frequency = frequency;
+		this->size = t;
 	}
-	int GetCoreCount()
+	Part* Clone() override
 	{
-		return this->corecount;
-	}
-	void SetCoreCount(int corecount)
-	{
-		this->corecount = corecount;
-	}
-	AbstractDevice* Clone() override
-	{
-		Processor * processor = new Processor;
-		*processor = *this;
-		return processor;
+		Clearence* eng = new Clearence;
+		*eng = *this;
+		return eng;
 	}
 };
 
-class VideoCard : public AbstractDevice
+class Wheels : public Part
 {
-	int volume;
+	int count;
 public:
-	int GetVolume()
+	int GetCount()
 	{
-		return this->volume;
+		return this->count;
 	}
-	void SetVolume(int volume)
+	void SetType(int t)
 	{
-		this->volume = volume;
+		this->count = t;
 	}
-	AbstractDevice* Clone() override
+	Part* Clone() override
 	{
-		VideoCard * videocard = new VideoCard;
-		*videocard = *this;
-		return videocard;
+		Wheels* eng = new Wheels;
+		*eng = *this;
+		return eng;
 	}
 };
 
-class Mainboard : public AbstractDevice
-{
-	double busfrequency;
-	VideoCard* videocard;
-public:
-	double GetBusFrequency()
-	{
-		return this->busfrequency;
-	}
-	void SetBusFrequency(double busfrequency)
-	{
-		this->busfrequency = busfrequency;
-	}
-	VideoCard* GetVideoCard()
-	{
-		return this->videocard;
-	}
-	void SetVideoCard(VideoCard* videocard)
-	{
-		this->videocard = videocard;
-	}
-	AbstractDevice* Clone() override
-	{
-		Mainboard *newObj = new Mainboard;
-		*newObj = *this;
-		newObj->videocard = this->videocard != nullptr? (VideoCard*) this->videocard->Clone() : nullptr;
-		return newObj;
-	}
-};
 
-/*
- Прототип (Prototype) порождающий шаблон проектирования, который предназначен для создания объекта
- через клонирование другого объекта вместо создания посредством конструктора.
-Паттерн используется чтобы:
--	избежать дополнительных усилий по созданию объекта стандартным путем (использование ключевого слова new,
-	когда создается не только сам объект, но и вся иерархия предков объекта),
-	когда это непозволительно дорого для приложения.
--	избежать наследования создателя объекта (object creator) в клиентском приложении, как это делает паттерн abstract factory.
-*/
 
 class Prototype abstract
 {
 public:
-	 virtual Prototype* Clone() abstract;
+	virtual Prototype* Clone() abstract;
 };
 
-class PersonalComputer: public Prototype
+class Car : public Prototype
 {
-	Box* box;
-	Mainboard* mainboard;
-	Processor* processor;
-	VideoCard* videocard;
-	vector<Memory*> memories;
-	vector<Hdd*> hdd;
+	Engine* engine1;
+	Wheels* wheels1;
+	Clearence* clierence1;
+	gearBox* gearbox1;
+	Fluel* fluel1;
+
 public:
-	Box* GetBox()
+	Engine* GetEngine()
 	{
-		return this->box;
+		return this->engine1;
 	}
-	void SetBox(Box* box)
+	void SetEngine(Engine* box)
 	{
-		this->box = box;
+		this->engine1 = box;
 	}
-	Processor* GetProcessor()
+	Wheels* GetProcessor()
 	{
-		return this->processor;
+		return this->wheels1;
 	}
-	void SetProcessor(Processor* processor)
+	void SetWheels(Wheels* w)
 	{
-		this->processor = processor;
+		this->wheels1 = w;
 	}
-	Mainboard* GetMainboard()
+	Clearence* GetClearencd()
 	{
-		return this->mainboard;
+		return this->clierence1;
 	}
-	void SetMainboard(Mainboard* mainboard)
+	void SetClierence(Clearence* c)
 	{
-		this->mainboard = mainboard;
+		this->clierence1 = c;
 	}
-	VideoCard* GetVideoCard()
+	Fluel* GetFluel()
 	{
-		return this->videocard;
+		return this->fluel1;
 	}
-	void SetVideoCard(VideoCard* videocard)
+	void SetFluel(Fluel* f)
 	{
-		this->videocard = videocard;
+		this->fluel1 = f;
 	}
-	vector<Memory*>& GetMemories()
+	gearBox* GetgearBox()
 	{
-		return memories;
+		return this->gearbox1;
 	}
-	void AddMemory(Memory * m)
+	void SetGearBox(gearBox* f)
 	{
-		memories.push_back(m);
+		this->gearbox1 = f;
 	}
-	vector<Hdd*>& GetHdd()
-	{
-		return hdd;
-	}
-	void AddHdd(Hdd * hdd)
-	{
-		this->hdd.push_back(hdd);
-	}
+
 	Prototype* Clone() override
 	{
-		PersonalComputer *pc = new PersonalComputer;
-		pc->box = this->box != nullptr ? (Box*)this->box->Clone(): nullptr;
-		pc->mainboard = this->mainboard != nullptr ? (Mainboard*)this->mainboard->Clone(): nullptr;
-		pc->processor = this->processor != nullptr ? (Processor*)this->processor->Clone() : nullptr;
-		pc->videocard = this->videocard != nullptr ? (VideoCard*)this->videocard->Clone() : nullptr;
-		for(Memory* m : this->memories)
-		{
-			if (m != nullptr)
-			{
-				pc->AddMemory((Memory*)m->Clone());
-			}
-		}
-		for (Hdd* h : this->hdd)
-		{
-			if (h != nullptr)
-			{
-				pc->AddHdd((Hdd*)h->Clone());
-			}
-		}		
-		return pc;
+		Car* car = new Car;
+		car->engine1 = this->engine1 != nullptr ? (Engine*)this->engine1->Clone() : nullptr;
+		car->fluel1 = this->fluel1 != nullptr ? (Fluel*)this->fluel1->Clone() : nullptr;
+		car->clierence1 = this->clierence1 != nullptr ? (Clearence*)this->clierence1->Clone() : nullptr;
+		car->gearbox1 = this->gearbox1 != nullptr ? (gearBox*)this->gearbox1->Clone() : nullptr;
+		car->wheels1 = this->wheels1 != nullptr ? (Wheels*)this->wheels1->Clone() : nullptr;
+
+		return car;
 	}
 };
 
-//	Класс палитры типичных конфигураций
-class PcPrototypes
-{
-	// Словарь, в котором хранятся прототипы конфигураций
-	map<string, PersonalComputer*> pcprototypes;
-public:
-	PcPrototypes()
-	{
-		// инициализация палитры прототипами конфигураций
-		InitializePcPrototypes();
-	}
 
-
-	// Доступ к прототипу конфигурации по его имени
-	PersonalComputer* operator[](string key)
-	{
-		return  pcprototypes[key];
-	}
-	void InitializePcPrototypes()
-	{
-		pcprototypes["Home"] = CreateHomeConfig();
-		pcprototypes["Office"] = CreateOfficeConfig();
-		pcprototypes["Game"] = CreateGamerConfig();
-	}
-
-	PersonalComputer* CreateHomeConfig()
-	{
-		PersonalComputer* config = new PersonalComputer();
-
-		Box* box = new Box();
-		box->SetColor("Silver");
-		config->SetBox(box);
-
-		Mainboard* mainboard = new Mainboard();
-		mainboard->SetProducer("ATI");
-		mainboard->SetBusFrequency(800);
-		mainboard->SetVideoCard(nullptr);
-		config->SetMainboard(mainboard);
-
-		Processor* processor = new Processor();
-		processor->SetProducer("Intel");
-		processor->SetCoreCount(2);
-		processor->SetFrequency(3);
-		config->SetProcessor(processor);
-
-		VideoCard* videocard = new VideoCard();
-		videocard->SetProducer("ATI");
-		videocard->SetVolume(1024);
-		config->SetVideoCard(videocard);
-
-		Hdd* hdd = new Hdd();
-		hdd->SetProducer("Samsung");
-		hdd->SetType("SATA");
-		hdd->SetVolume(500);
-		config->AddHdd(hdd);
-
-		Memory* memory = new Memory();
-		memory->SetType("DDR2");
-		memory->SetVolume(2);
-		config->AddMemory(memory);
-
-		return config;
-	}
-
-	PersonalComputer* CreateOfficeConfig()
-	{
-		PersonalComputer* config = new PersonalComputer();
-
-		Box* box = new Box();
-		box->SetColor("White");
-		config->SetBox(box);
-
-		Mainboard* mainboard = new Mainboard();
-		mainboard->SetProducer("Albatron");
-		mainboard->SetBusFrequency(800);
-
-		VideoCard* videocard = new VideoCard();
-		videocard->SetProducer("nVidia");
-		videocard->SetVolume(1024);
-		mainboard->SetVideoCard(videocard);
-		config->SetMainboard(mainboard);
-
-		Processor* processor = new Processor();
-		processor->SetProducer("AMD");
-		processor->SetCoreCount(1);
-		processor->SetFrequency(1.8);
-		config->SetProcessor(processor);
-
-		Hdd* hdd = new Hdd();
-		hdd->SetProducer("LG");
-		hdd->SetType("SATA");
-		hdd->SetVolume(160);
-		config->AddHdd(hdd);
-
-		Memory* memory = new Memory();
-		memory->SetType("DDR2");
-		memory->SetVolume(1);
-		config->AddMemory(memory);
-
-		config->SetVideoCard(nullptr);
-
-		return config;
-	}
-
-	PersonalComputer* CreateGamerConfig()
-	{
-		PersonalComputer* config = new PersonalComputer();
-
-		Box* box = new Box();
-		box->SetColor("Black");
-		config->SetBox(box);
-
-		Mainboard* mainboard = new Mainboard();
-		mainboard->SetProducer("Asus");
-		mainboard->SetBusFrequency(800);
-		mainboard->SetVideoCard(nullptr);
-		config->SetMainboard(mainboard);
-
-		Processor* processor = new Processor();
-		processor->SetProducer("Intel");
-		processor->SetCoreCount(4);
-		processor->SetFrequency(4);
-		config->SetProcessor(processor);
-
-		VideoCard* videocard = new VideoCard();
-		videocard->SetProducer("nVidia");
-		videocard->SetVolume(1024);
-		config->SetVideoCard(videocard);
-
-		Hdd* hdd = new Hdd();
-		hdd->SetProducer("WD");
-		hdd->SetType("SATA2");
-		hdd->SetVolume(1024);
-		config->AddHdd(hdd);
-
-		Memory* memory = new Memory();
-		memory->SetType("DDR2");
-		memory->SetVolume(4);
-		config->AddMemory(memory);
-
-		return config;
-	}
-};
-
-void PrintPc(PersonalComputer *pc)
-{
-	cout << "PC configuration: " << endl;
-	cout << "Box: " << pc->GetBox()->GetColor() << endl;
-	cout << "Mainboard: " << pc->GetMainboard()->GetProducer() << pc->GetMainboard()->GetBusFrequency() << " MHz" << endl;
-	if (pc->GetMainboard()->GetVideoCard() != nullptr)
-	{
-		cout << "Integrated VideoCard: " << pc->GetMainboard()->GetVideoCard()->GetProducer()
-			 << pc->GetMainboard()->GetVideoCard()->GetVolume() << " Mb" << endl;
-	}
-	cout << "Processor: " << pc->GetProcessor()->GetProducer()
-		 << pc->GetProcessor()->GetFrequency() << " GHz "
-		 << pc->GetProcessor()->GetCoreCount() << " core" << endl;
-	cout << "Memories: ";
-	for(Memory *m : pc->GetMemories())
-	{
-		cout << m->GetType() << "  " << m->GetVolume() << " Gb\n";
-	}
-	cout << "Hdds: ";
-	for(Hdd *h : pc->GetHdd())
-	{
-		cout << h->GetProducer() <<"  " << h->GetType() << "  " << h->GetVolume() << " Gb\n";
-	}
-	
-	if (pc->GetVideoCard() != nullptr)
-	{
-		cout << "VideoCard: ";
-		cout << pc->GetVideoCard()->GetProducer() << "  " << pc->GetVideoCard()->GetVolume() << " Mb\n";
-	}
-}
 
 int main()
 {
-	// Ввести имя прототипа конфигурации
-	cout << "Enter config prototype name: ";
-	string prototypeName;
-	cin >> prototypeName;
-	PcPrototypes pc;
+
+
 	// Получение прототипа конфигурации из палитры по его имени
-	PersonalComputer *prototype = pc[prototypeName];
+	Car* prototype = new Car();
+	Clearence* c = new Clearence();
+	prototype->SetClierence(c);
 	if (prototype != nullptr)
 	{
-		// Создание конфигурации на основе выбранного прототипа
-		PersonalComputer *pc = dynamic_cast<PersonalComputer*>(prototype->Clone());
-		// Вывод состава конфигурации на консоль
-		PrintPc(pc);
+		Car* car = dynamic_cast<Car*>(prototype->Clone());
 	}
 	else
 	{
